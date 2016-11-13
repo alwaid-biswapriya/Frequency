9d354ec4455e4b6725431509f30b93a2aa547d2c
biswapriya khawas <biswa.khawas@gmail.com>
2016-11-13 20:23:17 +0530

    My first test

---
+++ Frequency Project/src/firstTest.java
@@ -0,0 +1,47 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+
+public class firstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner sc = new Scanner(System.in);
+		String num = Integer.toString(sc.nextInt());
+		
+		
+		char numArray[] = num.toCharArray();
+		Integer count=0;
+		Map<Character,Integer> numMap = new TreeMap<Character,Integer>();
+		for(char c : numArray){
+			if(numMap.containsKey(c)){
+				numMap.put(c,numMap.get(c) +1 ) ;
+				
+						
+			}
+			else {
+			
+				numMap.put(c, 1);
+			}
+			
+			
+		}
+		
+		for(Map.Entry<Character, Integer> entry : numMap.entrySet()){
+			
+			System.out.println(entry.getKey() + "-->" + entry.getValue() );
+			
+			
+			
+		}
+		
+		
+
+	}
+
+}
